6770f2c26e86e079ae42b9a7d35396c327f300f1
Luan <ventobo123@gmail.com>
2024-10-13 02:45:20 -0300

    add QueryProvider with react-query client and login schema validation using Hono and Zod

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -16,22 +16,19 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import Link from "next/link";
+import { loginSchema } from "../schema";
 
-const formSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(1, "Required"),
-});
 
 export const SignInCard = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
     defaultValues: {
       email: "",
       password: "",
     },
   });
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
+  const onSubmit = (values: z.infer<typeof loginSchema>) => {
     console.log(values);
   };
 
