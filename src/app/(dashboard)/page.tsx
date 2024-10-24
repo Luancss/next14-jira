3c2f899fc5ae85413ae3c9200db8f27969027b7a
Luan <ventobo123@gmail.com>
2024-10-23 22:34:47 -0300

    Dashboard layout

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,4 @@
 import { getCurrent } from "@/features/auth/actions";
-import { UserButton } from "@/features/auth/components/user-button";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
@@ -8,10 +7,6 @@ export default async function Home() {
   if (!user) {
     redirect("/sign-in");
   }
-  
-  return (
-    <div>
-      <UserButton/>
-    </div>
-  );
+
+  return <div>This is a home page</div>;
 }
